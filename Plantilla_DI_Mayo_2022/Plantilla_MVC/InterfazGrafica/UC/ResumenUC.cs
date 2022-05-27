20a13dde61ca341782cf8735ec07bbf5c322db1c
jon <jfernandezdemera@birt.eus>
2022-05-27 18:04:10 +0200

    commit de examen final

---
+++ Plantilla_DI_Mayo_2022/Plantilla_MVC/InterfazGrafica/UC/ResumenUC.cs
@@ -0,0 +1,43 @@
+﻿using ObjetosTransferencia.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfazGrafica.UC
+{
+    public partial class ResumenUC : UserControl
+    {
+
+        private List<PedidoDTO> listaPedidos;
+
+        public ResumenUC(List<PedidoDTO> listaPedidos)
+        {
+            InitializeComponent();
+            double gastos = 0.0;
+
+            // TODO: Añade los campos que se piden al eje X e Y
+            List<DateTime> x1 = new List<DateTime>();
+            List<double> y1 = new List<double>();
+
+            for(int i = 0; i < listaPedidos.Count; i++)
+            {
+                x1.Add(listaPedidos[i].FechaPedido);
+                y1.Add(listaPedidos[i].PrecioEnvio);
+                gastos += listaPedidos[i].PrecioEnvio;
+            }
+
+            chart_Pedidos.Series["Gastos Envio"].Points.DataBindXY(x1 , y1);
+
+            // TODO: suma los gastos
+            lab_SumaEnvio.Text = "Suma Gastos:" + gastos;
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Fernandezdemera_Fuentes_Jon_ExmaneFinalDI
-Examen final DI
+# **Índice**
+1. [About](#id1)
+2. [Contact](#id2)
+3. [Licence](#id3)
+4. [Refenrece](#id4)
+
+## About
+Examen final del grado superior de desarrollo de aplicacion multiplataforma de la asignatura diseño de interfaces.
+## Contact
+Jon Fernández de Mera Fuentes
+Email: jfernandezdemera@birt.es
+## Licence
+Se utilizará la licencia de código libre sin ánimo de lucro.
+## References
+Se ha utilizado para su elaboración MVS2022 Comunity Edition.
+Sql Server Management Studio V18.1.1
